79a922a7a3106c0a5a63f01c65833852fdba81f9
Vadim-Galimov <115936408+Vadim-Galimov@users.noreply.github.com>
2025-09-20 14:50:01 +0500

    add db, show tasks from db

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from tasks.models import Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, "./core/index.html")
+    tasks = Task.objects.all()
+
+    return render(request, "./core/index.html", {'tasks': tasks})
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название задачи")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    is_completed = models.BooleanField(default=False, verbose_name="Выполнено")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name= "Задача"
+        verbose_name_plural = "Задачи"
