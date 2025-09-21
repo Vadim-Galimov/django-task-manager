0678b8919b1c28d8511f0a45d6e96799d350d557
Vadim-Galimov <115936408+Vadim-Galimov@users.noreply.github.com>
2025-09-21 20:23:33 +0500

    delete_task work

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index , name='home'),
     path('all_tasks', views.all_tasks, name='all_tasks'),
     path('add_task', views.add_task, name='add_task'),
+    path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from tasks.models import Task
 from tasks.forms import TaskCreateForm
 
@@ -26,3 +26,9 @@ def add_task(request):
         form = TaskCreateForm()
 
     return render(request, "./core/add_task.html", {'form': form})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('all_tasks')
